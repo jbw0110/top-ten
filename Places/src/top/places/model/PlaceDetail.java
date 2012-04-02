dad4347eaac66de4dc29f2d97c813f281127f7d2
Joseph <Joseph@Joseph-PC>
2012-04-02 15:33:12 -0500

    No errors, but crashes

---
+++ Places/src/top/places/model/PlaceDetail.java
@@ -0,0 +1,18 @@
+package top.places.model;
+
+import com.google.api.client.util.Key;
+
+public class PlaceDetail {
+
+	@Key
+	public Place result;
+
+	@Override
+	public String toString() {
+		if (result!=null) {
+			return result.toString();
+		}
+		return super.toString();
+	}
+}
+
